a31ce32c4dabb9fe553cc0bee2b9a65eccbaca34
장혜민 <gpals5987@gmail.com>
2024-08-20 20:25:48 +0900

    [refactor] MenuRepo import 추가

--- src/main/java/com/rgt/repository/MenuRepository.java
+++ src/main/java/com/rgt/repository/MenuRepository.java
@@ -1,6 +1,7 @@
 package com.rgt.repository;
 
 import com.rgt.entity.Cafe;
+import com.rgt.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
