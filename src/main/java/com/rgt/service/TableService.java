2a0f1fa377146cfff6ff385a8f348ca2dfc4ee48
장혜민 <gpals5987@gmail.com>
2024-08-21 13:36:48 +0900

    [feat] 임시 redisService 삭제

--- src/main/java/com/rgt/service/TableService.java
+++ src/main/java/com/rgt/service/TableService.java
@@ -1,14 +1,11 @@
 package com.rgt.service;
 
-import com.rgt.repository.CafeRepository;
-import com.rgt.repository.TableRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class TableService {
-    private final TableRepository tableRepository;
 
 
 }
