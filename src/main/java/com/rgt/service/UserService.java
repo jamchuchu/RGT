9b2e12a5d8a1f033edc8b46e77dca91926c2eaf8
장혜민 <gpals5987@gmail.com>
2024-08-21 14:17:09 +0900

    [feat] userDetail, userDetail service 설정
    user -> userDetail impl
    userService -> userDetailService impl

--- src/main/java/com/rgt/service/UserService.java
+++ src/main/java/com/rgt/service/UserService.java
@@ -1,15 +1,38 @@
 package com.rgt.service;
 
+import com.rgt.constants.Authority;
+import com.rgt.entity.User;
 import com.rgt.repository.CafeRepository;
 import com.rgt.repository.UserRepository;
+import com.rgt.request.UserReqDto;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
 
-    //로그인
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserByUsername(username);
+    }
+
+    //일반 회원가입
+    public UserDetails saveUserForUser(UserReqDto reqDto){
+        User user = User.of(reqDto.getUserName(), reqDto.getPassword(), Authority.USER);
+        return user;
+    }
+
+    //사장 회원가입
+    public UserDetails saveUserForOwner(UserReqDto reqDto){
+        User user = User.of(reqDto.getUserName(), reqDto.getPassword(), Authority.CAFE_OWNER);
+        return user;
+    }
+
 
 }
